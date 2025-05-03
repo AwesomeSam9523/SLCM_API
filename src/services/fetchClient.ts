1836c0efda86d7ce9e12c7e107805d3c85dc4ac5
AwesomeSam9523 <samakshgupta04@gmail.com>
2025-05-03 07:48:09 +0530

    Convert to ts and add database logic

--- src/services/fetchClient.ts
+++ src/services/fetchClient.ts
@@ -3,8 +3,9 @@ import fetchCookie from 'fetch-cookie';
 import { CookieJar } from 'tough-cookie';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-export const cookieJar = new CookieJar();
 export const proxyAgent = new HttpsProxyAgent('http://127.0.0.1:5559');
-export const fetchWithCookies = fetchCookie(fetch, cookieJar);
+export async function fetchWithCookies(cookieJar: CookieJar) {
+  return fetchCookie(fetch, cookieJar);
+}
---
+++ src/utils/response.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+function successJson(res: express.Response, data: any | undefined = undefined) {
+  res.status(200).json({
+    success: true,
+    data,
+  });
+}
+
+function errorJson(res: express.Response, errorCode: number, message: string) {
+  res.status(errorCode).json({
+    success: false,
+    message,
+  });
+}
+
+export {successJson, errorJson};
